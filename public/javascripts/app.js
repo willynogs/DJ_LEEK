e9851866a0accd20b4e8233858069264616c85e2
Will Naugle <willynogs@Wills-iMac.local>
2017-09-08 17:58:25 -0400

    add video home

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,12 +2,3 @@
 console.log('%c//--\n\tmade by @willynogs in columbus, ohio\n\t[willynogs.com]\n--//', 'background: black; color: lime;');
 
 //-- add click event for menu
-var t = document.getElementById('nav');
-var ti = document.getElementById('nav-menu-inner');
-var m = document.getElementById('nav-overlay');
-t.addEventListener('touchend', handleClick, false);
-//t.addEventListener('click', handleClick, false);
-function handleClick(e) {
-  ti.classList.toggle('nav-menu-active');
-  m.classList.toggle('nav-overlay-show');
-}
